4da088f91936d64e1a58fde1b5ab9d3ecc9088d0
Kevin <kl29@princeton.edu>
2020-08-28 21:41:30 -0500

    Updates

---
+++ precache-manifest.94fef5760ffc9a8db8adea4d7771540a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891fc819b8c03ed6e1439f97b7f36816",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02ab8ab8b5ca51feed5a",
+    "url": "/static/css/3.c7b31c95.chunk.css"
+  },
+  {
+    "revision": "68b85de8daff2d2a9bf7",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "dea9405345b55be6eaad",
+    "url": "/static/css/main.4b0ab329.chunk.css"
+  },
+  {
+    "revision": "5acc232fdd03a98d3011",
+    "url": "/static/js/2.fcdc6190.chunk.js"
+  },
+  {
+    "revision": "02ab8ab8b5ca51feed5a",
+    "url": "/static/js/3.8af5f7fb.chunk.js"
+  },
+  {
+    "revision": "68b85de8daff2d2a9bf7",
+    "url": "/static/js/4.c75a171b.chunk.js"
+  },
+  {
+    "revision": "dea9405345b55be6eaad",
+    "url": "/static/js/main.a9a71693.chunk.js"
+  },
+  {
+    "revision": "cafab6e851a797974c68",
+    "url": "/static/js/runtime-main.53e66b6f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8c7244f2b72da2f3ac634ab98d287125",
+    "url": "/static/media/bsa.8c7244f2.gif"
+  },
+  {
+    "revision": "f35d8914329fc3d1140adcfeae1f30dc",
+    "url": "/static/media/cnt.f35d8914.jpg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "f8c72d8fcef03c83aa6251f1bb8fcaae",
+    "url": "/static/media/dimare.f8c72d8f.png"
+  },
+  {
+    "revision": "621fff03cbd0d29819dfb12e561e0576",
+    "url": "/static/media/fs.621fff03.png"
+  },
+  {
+    "revision": "722a230c7d9da2a05d05fe4366018a30",
+    "url": "/static/media/fusion.722a230c.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "48597878a00e805ad5ee7e527dc01b2f",
+    "url": "/static/media/sv.48597878.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "6b706728b533f61e93049aa714143883",
+    "url": "/static/media/tm.6b706728.jpg"
+  },
+  {
+    "revision": "4eba4a051737008bdacdc472a018bc87",
+    "url": "/static/media/tt.4eba4a05.png"
+  }
+]);
