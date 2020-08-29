22fee22d9d87985bbe05d601b84161f8e220b99c
Kevin Lin <kl29@princeton.edu>
2020-08-29 18:09:58 -0500

    Updates

---
+++ precache-manifest.122c3f79600b3430652e14f9bb515afe.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e24aaf1e8ade24ad9f5ffc6da2a927b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea7d0da6851245efeb35",
+    "url": "/static/css/3.c7b31c95.chunk.css"
+  },
+  {
+    "revision": "7d175afc925a4122f607",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "02ca8bb089ed9e056f3e",
+    "url": "/static/css/main.7f096d13.chunk.css"
+  },
+  {
+    "revision": "abf35faeba8f620ec619",
+    "url": "/static/js/2.8d6bbc06.chunk.js"
+  },
+  {
+    "revision": "ea7d0da6851245efeb35",
+    "url": "/static/js/3.c336a6f0.chunk.js"
+  },
+  {
+    "revision": "7d175afc925a4122f607",
+    "url": "/static/js/4.aca1e521.chunk.js"
+  },
+  {
+    "revision": "02ca8bb089ed9e056f3e",
+    "url": "/static/js/main.3dcbea61.chunk.js"
+  },
+  {
+    "revision": "878b506945b00ee758b6",
+    "url": "/static/js/runtime-main.1727ebd6.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f35d8914329fc3d1140adcfeae1f30dc",
+    "url": "/static/media/cnt.f35d8914.jpg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "f8c72d8fcef03c83aa6251f1bb8fcaae",
+    "url": "/static/media/dimare.f8c72d8f.png"
+  },
+  {
+    "revision": "90b9d89402d354956000f15f12ec0243",
+    "url": "/static/media/dj.90b9d894.png"
+  },
+  {
+    "revision": "40d8a41b171662a1c28a327f55b7edbd",
+    "url": "/static/media/ea.40d8a41b.png"
+  },
+  {
+    "revision": "621fff03cbd0d29819dfb12e561e0576",
+    "url": "/static/media/fs.621fff03.png"
+  },
+  {
+    "revision": "722a230c7d9da2a05d05fe4366018a30",
+    "url": "/static/media/fusion.722a230c.png"
+  },
+  {
+    "revision": "1162b21882db8f30bd4a1bf0ac7e6f68",
+    "url": "/static/media/gre.1162b218.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "4cbb3df1ed8738fb15f831f66bb51ba9",
+    "url": "/static/media/mnist.4cbb3df1.png"
+  },
+  {
+    "revision": "12535d3edd002cdcbb0f275ec7a6a4de",
+    "url": "/static/media/ppt.12535d3e.jpg"
+  },
+  {
+    "revision": "d62d7230dfa71c8b805b018a32e9c363",
+    "url": "/static/media/r.d62d7230.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "c7a11a34b7674f6eb7a6c3ef7b092544",
+    "url": "/static/media/ssss.c7a11a34.png"
+  },
+  {
+    "revision": "f7a331ff515d5bbfcc790fc6e59b8fd5",
+    "url": "/static/media/svd.f7a331ff.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "6b706728b533f61e93049aa714143883",
+    "url": "/static/media/tm.6b706728.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94fef5760ffc9a8db8adea4d7771540a.js"
+  "/precache-manifest.122c3f79600b3430652e14f9bb515afe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
